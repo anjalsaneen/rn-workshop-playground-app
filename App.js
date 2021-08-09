1e6d1a9456615fcb347f6a4794f0c2b443b48f53
Anjal Saneen <anjalsaneen@Anjals-MacBook-Pro.local>
2021-08-09 15:22:33 +0530

    Added Flatlist

--- App.js
+++ App.js
@@ -3,14 +3,41 @@ import {
   View,
   Text,
   StyleSheet,
-  Button
+  Button,
+  FlatList,
+  PermissionsAndroid
 } from 'react-native';
+import Contacts from 'react-native-contacts';
 
 const App = () => {
   const [count, setCount] = useState(0)
+  const [contacts, setContacts] = useState([])
+
+  const getContacts= () => {
+    console.log('Test')
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+      {
+        'title': 'Contacts',
+        'message': 'This app would like to view your contacts.',
+        'buttonPositive': 'Please accept bare mortal'
+      }
+    )
+    .then((item) => {
+      Contacts.getAll().then(contacts => {
+        console.log({contacts})
+        setContacts(contacts)
+      })
+    })
+  }
+
+  const renderItem = ({ item }) => (
+    <Text style={{paddingVertical: 10}}>{item.displayName}</Text>
+  );
 
   return (
-    <View style={styles.container}>
+    <View style={{flex: 1}}>
+      <View style={styles.container}>
       <Button
         title="  +   "
         onPress={() => setCount(count+1)}
@@ -20,6 +47,22 @@ const App = () => {
         title="  -   "
         onPress={() => setCount(count-1)}
       />
+      </View>
+
+      <View style={{padding: 16}}>
+        <Text style={{fontWeight: '700'}}>Contacts</Text>
+        <Button
+        title="Get Contacts"
+        onPress={() => getContacts()}
+        />
+
+      <FlatList
+        data={contacts}
+        renderItem={renderItem}
+        keyExtractor={item => item.displayName}
+      />
+
+      </View>
     </View>
   )
 }
